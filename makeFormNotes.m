4214289a79486346d3bba8231f4b87483b22f876
nesh170 <sivaneshwaran.loganathan@duke.edu>
2015-01-28 19:26:58 -0500

    Added FreqMultiplier

--- makeFormNotes.m
+++ makeFormNotes.m
@@ -6,6 +6,7 @@
 function [noteVector] = makeFormNotes(duration,freq)
 samplingf=8000;
 t=0:(1/samplingf):(duration-1/samplingf);
-noteVector = exp(-t).*(cos(2*pi*((220*(2^(freq/12)).*t))));
+FreqMultiplier=1; %Increasing this causes higher pitch sounds, decreasing causes lower pitched sounds
+noteVector = exp(-t).*(cos(2*pi*FreqMultiplier*((220*(2^(freq/12)).*t))));
 end
 
